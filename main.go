e937953f1d95cf80ab4604d66b01234c118d4d81
Zaid Khan <zaiddkhhan@gmail.com>
2024-10-13 15:14:28 +0530

    peers and server setup done

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"net"
+)
+
+const defaultListenAdds = ":5001"
+
+type Server struct {
+	Config
+	peers     map[*Peer]bool
+	ln        net.Listener
+	addPeerCh chan *Peer
+	quitCh    chan struct{}
+	msgCh     chan []byte
+}
+
+type Config struct {
+	ListenAddr string
+	ln         net.Listener
+}
+
+func NewServer(cfg Config) *Server {
+	if len(cfg.ListenAddr) == 0 {
+		cfg.ListenAddr = defaultListenAdds
+	}
+	return &Server{
+		Config:    cfg,
+		peers:     make(map[*Peer]bool),
+		addPeerCh: make(chan *Peer),
+		quitCh:    make(chan struct{}),
+		msgCh:     make(chan []byte),
+	}
+}
+
+func (s *Server) loop() {
+	for {
+		select {
+		case rawMsg := <-s.msgCh:
+			fmt.Println(rawMsg)
+		case <-s.quitCh:
+			return
+		case peer := <-s.addPeerCh:
+			s.peers[peer] = true
+
+		}
+	}
+}
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.ListenAddr)
+	if err != nil {
+		return err
+	}
+	s.ln = ln
+	go s.loop()
+	slog.Info("server running on", s.ListenAddr)
+	return s.acceptLoop()
+
+}
+
+func (s *Server) acceptLoop() error {
+
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			slog.Error("accept error:", err)
+			continue
+		}
+		go s.handleConn(conn)
+
+	}
+}
+
+func (s *Server) handleConn(conn net.Conn) {
+	peer := NewPeer(conn, s.msgCh)
+	s.addPeerCh <- peer
+	slog.Info("new peer connected:", "remote addr", conn.RemoteAddr())
+
+	if err := peer.readLoop(); err != nil {
+		slog.Error("peer read error:", err)
+	}
+}
+
+func main() {
+	server := NewServer(Config{})
+	log.Fatal(server.Start())
+}
